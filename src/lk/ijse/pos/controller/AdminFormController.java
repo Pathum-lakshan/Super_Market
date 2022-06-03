aca5b477cbef3d0fde028c143958be8ac5900248
lakshanheratg <lakshanherath1101@gmail.com>
2022-06-03 12:58:33 -0700

    complete create manage form

---
+++ src/lk/ijse/pos/controller/AdminFormController.java
@@ -0,0 +1,10 @@
+/**
+ * @author     : ALE_IS_TER
+ * Project Name: Layered Architecture
+ * Date        : 6/3/2022
+ * Time        : 12:28 PM
+ *@Since       : 0.1.0
+ */
+
+package lk.ijse.pos.controller;public class AdminFormController {
+}
